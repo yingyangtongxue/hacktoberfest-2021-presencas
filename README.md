8d1c862cfa768d5d31d1fa35a191729c62ace230
Yudi <renanyaguinuma@hotmail.com>
2021-10-03 22:35:58 -0300

    Importar arquivos
    Arquivos do repositório fct-coders/hacktoberfest-2020-presencas

---
+++ README.md
@@ -0,0 +1,29 @@
+# Presenças do Hacktoberfest 2020
+
+![](imgForReadme.png)
+
+Esse repo contém o código fonte para o livro de presenças do Hacktoberfest 2020, disponível em [http://fct-hacktober2020.surge.sh/](http://fct-hacktober2020.surge.sh/).
+
+## Instalação para desenvolvimento
+
+Instale o pacote serve através de
+
+```bash
+npm install -g serve
+```
+
+Execute
+
+```bash
+serve .
+```
+
+Acesse http://localhost:5000
+
+## Contribuições
+
+Pull requests são bem vindas, por favor crie um issue para discutir a mudança antes de realizá-la. Lembre-se de ler as [guidelines de contribuição](CONTRIBUTING.md) e o nosso [Código de Conduta](CODE_OF_CONDUCT.md).
+
+## Licença
+
+Esse projeto segue a licença [MIT](https://choosealicense.com/licenses/mit/).
---
+++ script.js
@@ -0,0 +1,78 @@
+//import contributors from "./data/_contributors.json"
+
+const API_URL = 'https://api.github.com/users/';
+
+const contributor_section = document.getElementById('section-contributors');
+
+//const contributors = [
+//  {
+//    username: 'deadpyxel',
+//    about: 'Hacktoberfest boys',
+//    languages: ['Python', 'Go', 'Rust'],
+//    year: '2012',
+//  },
+//];
+
+const get_user_data = async (username) => {
+  const resp = await fetch(API_URL + username);
+  return await resp.json();
+};
+
+const make_contributor = async (user) => {
+  const user_data = await get_user_data(user.username);
+  const contributor = {
+    ...user,
+    avatar_url: user_data.avatar_url,
+    name: user_data.name,
+  };
+  return contributor;
+};
+
+const create_contributor_card = (user) => {
+  let lang_list = '';
+  user.languages.forEach((lang) => {
+    lang_list += `<li class="lang">${lang}</li>`;
+  });
+  const cardHTML = `
+            <div>
+                <img class="avatar" src="${user.avatar_url}" alt="${
+    user.name
+  }" />
+            </div>
+            <div class="user-info">
+                <h2>${user.name || user.username}</h2>
+                <p>"${user.about}"</p>
+                <p>Ano: ${user.year}</p>
+
+                <ul class="languages">
+                  ${lang_list}
+                </ul>
+            </div>
+    `;
+  let card = document.createElement('div');
+  card.className = 'card';
+  card.innerHTML = cardHTML;
+  contributor_section.appendChild(card);
+};
+
+const list_contributors = () => {
+  fetch('./data/_contributors.json')
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach(async (contributor) => {
+        let contrib = await make_contributor(contributor);
+        create_contributor_card(contrib);
+      });
+    });
+};
+
+list_contributors();
+
+function abrirMenu() {
+  var nav = document.getElementById('nav-wrapper');
+  if (nav.className === 'nav-wrapper') {
+    nav.className += ' responsive';
+  } else {
+    nav.className = 'nav-wrapper';
+  }
+}
